fb9eb19bddfb9934b3338f3f3bd17daa00d146a7
ihsan <ihsan@kominfo.surabaya.go.id>
2022-05-17 12:40:50 +0700

    first commit

---
+++ src/components/WishesSection/wishlist-data.js
@@ -0,0 +1,197 @@
+
+import prs from './assets/prs.jpg';
+import ars from './assets/ars.jpg';
+import taz from './assets/taz.jpg';
+import dewi from './assets/dewi.jpg';
+import joyo from './assets/joyo.jpeg';
+import firly from './assets/firly.jpeg';
+import hurin from './assets/HURIN.jpg';
+import marini from './assets/MariniAmelia.jpg';
+import nurul from './assets/nurul.jpeg';
+import suju from './assets/suju.jpeg';
+import titus from './assets/titus.jpg';
+import efriza from './assets/efriza.jpg';
+import kres from './assets/kres.jpg';
+import litak from './assets/litak.jpg';
+import oc from './assets/oc.jpg';
+import tegal from './assets/tegal.jpg';
+import ve from './assets/ve.jpg';
+import esva from './assets/esva.jpeg';
+
+
+
+
+
+// import  from './assets/';
+
+
+import xxx from './assets/f.png';
+
+export const wishlist = [
+  {
+    name: 'Prastiyo',
+    infoName: 'Sahabat',
+    description: `selamat menempuh hidup baru Zulvia dan mas Ihsan semoga menjadi pasangan teman hidup selamanya sesurga, enjoy your little family always be happy my sweet couple!🥰🤗 amin`,
+    image: prs,
+  },
+  {
+    name: 'Nurul',
+    infoName: 'Teman JKG',
+    description: `Happy Wedding kak Zulvia dan mas Ihsan 💐
+    Selamat menyempurnakan setengah perjalanan agama dan kehidupan baru kak. Berbahagialah kak julku.Till jannah aamiin`,
+    image: nurul,
+  },
+  {
+    name: 'suju yang cantik',
+    infoName: 'Teman SMA',
+    description: `Selamat menikah, teman! Semoga pasanganmu sabar menghadapi tingkah laku ajaibmu, ya.`,
+    image: suju,
+  },
+
+  {
+    name: 'Mochammad Arsyad',
+    infoName: 'Konco Plek, Dolen, Mabar',
+    description: `🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰
+    Alhamdulillah, Samawa ges 😁👍🏼
+    🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰
+    🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳`,
+    image: ars,
+  },
+
+  {
+    name: 'Tazkya',
+    infoName: 'Teman Kuliah',
+    description: 'Selamat menjalani fase kehidupan yang baru zulvia dan ihsan. Semoga lebih bahagia dan 2 keponakan cukuplah ya.',
+    image: taz,
+  },
+
+  {
+    name: 'Fitria',
+    infoName: 'Teman Kuliah',
+    description: `Happy wedding yayukkuuu 😘 alhamdulillah setelah melewati berbagai macam lika liku kehidupan ya yukk wkwk barakallah 🤗 semoga lancar ya sampek hari H, semoga selalu diberi keberkahan dan tentunya jadi keluarga yg sakinah mawaddah warahmah 🤲🏻❤️ aamiiinnn`,
+    image: dewi,
+  },
+
+
+  {
+    name: 'Joyo',
+    infoName: 'Sahabat',
+    description: `Tidak ada keluarga yg sempurna, nanti terkadang kalian akan berdebat, berkelahi dan bahkan satu waktu berhenti berbicara satu sama lain. Namun pada akhirnya kalian bisa saling mengerti & memaafkan karena keluarga tetaplah keluarga dimana cinta akan selalu ada.`,
+    image: joyo,
+  },
+  {
+    name: 'Marimar cantiiik',
+    infoName: 'Teman kamar 5',
+    description: `Happy wedding kakak 🤗😘
+    Baarakallahu laka wa baarakaa alaika wa jamaa bainakumaa fii khoirin..
+    Semoga kakak menjadi pasangan yang sejati dunia akhirat, semoga dikaruniai keturunan yang saleh dan salehah, semoga jadi keluarga yang sakinah, mawaddah dan warahmah
+    Aamiin ya robbal'alamiin ❤❤❤`,
+    image: marini,
+  },
+
+  {
+    name: 'Adek Hurin',
+    infoName: 'Adik Unyuk',
+    description: `Assalamualaikum, hai jul(kipli). Alhamdulillah yah, setelah sekian lama berpuasa. Kini saatnya berbuka puasa dari kehausanmu dalam dunia asmara 🙊
+    Dalam setiap waktu yang akan kalian lakukan berdua, tentunya akan banyak menemukan nano nano dalam hiruk piruk rumah tetangga, maksd saya rumah tangga, Sorry. Haha
+    Doaku, semoga perjalanan kalian selalu diberikan kemudahan, keberkahan, ketenangan, dan kebahagiaan. Sakinah Mawadah warahmah untuk kalian.
+    So sweet kan kata² dariku, 😘.
+    
+    With Hate
+    Adek Hurin si Unyuk🤙`,
+    image: hurin,
+  },
+
+  {
+    name: 'Gloxynia',
+    infoName: 'Teman',
+    description: `Alhamdulillah, akhir e nikah, mugo-mugo lancar acarane tekan awal sampek akhir, awal iku akad nikah akhir iku maut(kematian), mugo-mugo nikah e diparingi berkah. Ntapss`,
+    image: titus,
+  },
+
+
+  {
+    name: 'Firly',
+    infoName: ' sahabat JKG mojosari',
+    description: `Wishing you happiness, full of love, and joy on your wedding day 🤍`,
+    image: firly,
+  },
+
+   {
+    name: 'Efriza',
+    infoName: 'Teman Kantor',
+    description:`Happy wedding mas ihsan dan istri. Semoga diberikan kelancaran, serta dijadikan keluarga yang samawa. Aamiiin    `,
+    image: efriza,
+  },
+
+  {
+    name: 'TEGAL',
+    infoName: 'KONTRAKAN BAROKAH ',
+    description:`Mbiyen asal koe seneng, lakonono.. saiki asal bojo seneng lakonono... Samawa pakde 🙏🏻`,
+    image: tegal,
+  },
+
+  {
+    name: 'Muhammad Kresna',
+    infoName: 'Rekan Kerja',
+    description:`Selamat Menikah mas 👏 Semoga menjadi keluarga yang Sakinah Mawaddah Warohmah... Aaminn.. 🙏
+
+    وَمِنْ آيَاتِهِ أَنْ خَلَقَ لَكُمْ مِنْ أَنْفُسِكُمْ أَزْوَاجًا لِتَسْكُنُوا إِلَيْهَا وَجَعَلَ بَيْنَكُمْ مَوَدَّةً وَرَحْمَةً إِنَّ فِي ذَلِكَ لَآيَاتٍ لِقَوْمٍ يَتَفَكَّرُونَ
+    
+    “Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang. Sesungguhnya pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir.” QS Ar Rum Ayat 21
+    `,
+    image: kres,
+  },
+
+  {
+    name: 'Ochim Cantik',
+    infoName: 'Babu',
+    description:`Alhamdulillah akhirnya ada yg mau sama Ihsan wkwkwk, semoga bahagia untuk kalian berdua, penuh berkah pernikahannya, dan mampu menghadapi ujian kedepannya. Numpang ngiklan, barangkali ada yg mau nemenin saya ke kondangannya, nanti ditraktir janji jiwa 😊🙏
+    `,
+    image: oc,
+  },
+
+
+    {
+    name: 'Nurrohmah hidayati yang baik hati dan tidak sombong',
+    infoName: 'Majikan yang baik hati',
+    description:`Selamat menempuh hidup baru pada tanggal nya 🤗 berkah untuk semua dilancarkan dan diberikan kemudahan, wis iku ae dungone cukup kan ?
+.
+Selamat bersakit2 untuk aksi pertama nya , setelah itu akan ena ena di aksi2 berikutnya 👩‍❤️‍💋‍👨👩‍❤️‍💋‍👨 
+`,
+    image: xxx,
+  },
+
+ {
+    name: 'Ve',
+    infoName: 'Teman SMK',
+    description:`Happy Wedding yaa chan🐛🤝🏼👩‍❤️‍👨🌹setelah sekian purnama akhirnya akan menuju ke jenjang kehidupan yg sesungguhnya.. 🤣. Pesanku  jadilah suami juga bapak yg  ichan yg sabar,telaten dan tanggung jawab. 
+    Semoga segala urusan dan acaranya dilancarkan, dan semoga sakinah mawaddah warohmah 👩‍❤️‍👨🐛aamiin `,
+    image: ve,
+  },
+
+ {
+    name: 'Litak & Amre',
+    infoName: 'Teman Kuliah & Kerja',
+    description:`Barakallah.. Happy wedding Ihsan & Zul.. Akhirnya setelah sekian lama berhubungan dan sekarang resmi juga jadi pasangan halal.. 🥰  Wishing you both a long future together filled with love and laughter. Enjoy your journey guys!! ❤️
+    `,
+    image: litak,
+  },
+
+  
+   {
+    name: 'ESVA DESKY ',
+    infoName: 'TEMAN GOES',
+    description:`SELAMAT MENEMPUH HIDUP BARU SEMOGA SAMAWA TILL JANNAH EAAA MBAK JULAIKAH
+    momongan tolong di lounchingkan segera 🥰`,
+    image: esva,
+  },
+
+  // {
+  //   name: '',
+  //   infoName: '',
+  //   description:``,
+  //   image: xxx,
+  // },
+
+];
